959362e803556fe395a680ba77029e251d6b4c42
albert chen <k60513albert@yahoo.com.tw>
2022-02-21 00:13:11 +0800

    init

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <Text>數位二甲</Text>
+      <Text>陳威誠</Text>
+      <Text>110919008</Text>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
